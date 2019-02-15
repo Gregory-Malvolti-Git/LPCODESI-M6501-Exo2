a3c23bbd41b7ef6da78dcc45cc3d216266ec55cb
m16009111 <m16009111@a-f004245.salsa.univ-amu.fr>
2019-02-15 15:30:29 +0100

    Modification du Main

--- TP2Exo5/src/Main.java
+++ TP2Exo5/src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CarteCredit ccredit = new CarteCredit(6000, 5000);
-		System.out.println("Carte Crédit autorise Paiement : " + ccredit.autorisePaiement(200));
+		System.out.println("Carte Crédit autorise Paiement (200) : " + ccredit.autorisePaiement(200));
 		Compte compteassoc = new Compte(1000);
 		CarteDébit cdebit = new CarteDébit(compteassoc);
-		System.out.println("Carte Débit autorise Paiement" + cdebit.autorisePaiement(200));
+		System.out.println("Carte Débit autorise Paiement (200) : " + cdebit.autorisePaiement(200));
 		
 	}
 
