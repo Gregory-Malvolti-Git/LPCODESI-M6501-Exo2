4d32ff8cf39da3d47548c9a349d0d3e1950f0fdb
m16009111 <m16009111@a-f004245.salsa.univ-amu.fr>
2019-02-15 15:20:51 +0100

    Classe CarteCredit

--- TP2Exo5/src/CarteCredit.java
+++ TP2Exo5/src/CarteCredit.java
@@ -3,6 +3,12 @@ public class CarteCredit extends CartePaiement {
 	protected double plafond;
 	protected double encours;
 	
+	public CarteCredit(double plafond, double encours) {
+		super();
+		this.plafond = plafond;
+		this.encours = encours;
+	}
+
 	public boolean autorisePaiement(double montant) {
 		return (this.encours + montant)<=plafond;
 	}
