639a5ec57179e66ad06f1ce27928230ac64fee02
m16009111 <m16009111@a-f004245.salsa.univ-amu.fr>
2019-02-15 15:01:41 +0100

    Ajout de la classe abstraite CartePaiement.

---
+++ TP2Exo5/src/CartePaiement.java
@@ -0,0 +1,10 @@
+
+public abstract class CartePaiement {
+	public void payeAchat(double montant) {
+		System.out.println("Vous avez payez : "+ montant);
+	}
+	
+	public void autorisePaiement(double montant) {
+	};
+
+};
